68268de392db42d14da90f2a5cb6fd20a9a14dae
Alex Perez <alexgp@unknown020fb5692481.attlocal.net>
2023-06-30 12:05:17 -0500

    another fix

--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -1,8 +1,7 @@
 import { columns } from "@/components/applicationsPage/columns"
 import { DataTable } from "@/components/applicationsPage/data-table"
 import { getApplications } from "@/lib/queries/application"
-import { getErrorMessage } from "@/lib/utils/errorMessage"
-import type { Application, ApplicationWithId } from "@/lib/utils/types"
+import type { Application, ApplicationWithId, Status } from "@/lib/utils/types"
 
 import { ApplicationDialouge } from "../../components/applicationsPage/applicationDialog"
 
@@ -10,27 +9,22 @@ export default async function ApplicationsPage() {
 
   const { applications } = await getApplications() 
 
+  const transformedApplications: Application[] = applications?.map((application) => ({
+    company: application.company,
+    applied_date: application.applied_date,
+    position: application.position,
+    location: application.location,
+    salary: application.salary,
+    application_type: application.application_type,
+    cover_letter: application.cover_letter,
+    status: application.status as Status
+  })) ?? [];
+
   
   return (
     <div className="container mx-auto py-10">
       <ApplicationDialouge buttonName="add application" title="Add an Application" description="Fill out the following form"/>
-      {applications ? <DataTable columns={columns} data={applications} /> : <div>Error fetching data</div>}
+      {applications ? <DataTable columns={columns} data={transformedApplications} /> : <div>Error fetching data</div>}
     </div>
   )
 }
-
-
- // const apps: Application[] = applications?.map((app) => {
-  //   return {
-  //     company: app.company,
-  //     applied_date: app.applied_date,
-  //     position: app.position,
-  //     location: app.location,
-  //     salary: app.salary,
-  //     application_type: app.application_type,
-  //     cover_letter: app.cover_letter,
-  //     status: app.status,
-  //   }
-  // })
-
-  // console.log(applications)
