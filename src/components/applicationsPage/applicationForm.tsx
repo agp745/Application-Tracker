567e53b703d81839ce8e65e6eed046c67405a7c3
Alex Perez <alexgp@unknown020fb5692481.attlocal.net>
2023-07-14 13:31:01 -0500

    application submit toast restyled

--- src/components/applicationsPage/applicationForm.tsx
+++ src/components/applicationsPage/applicationForm.tsx
@@ -83,12 +83,8 @@ export function ApplicationForm() {
       form.reset()
     
       toast({
-        title: "You submitted the following application",
-        description: (
-            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-              <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-            </pre>
-        ),
+        className: 'bg-black border-green-700',
+        title: "Application submitted sucessfully",
       })
     }
 
