2840b04a749673e567b4d732c1dea6e07e14a4ba
Alex Perez <alexgp@Alexs-MBP.attlocal.net>
2023-08-11 12:44:07 -0500

    column names fix

--- src/components/applicationsPage/data-table.tsx
+++ src/components/applicationsPage/data-table.tsx
@@ -106,7 +106,7 @@ export function DataTable<TData, TValue>({ columns,data }: DataTableProps<TData,
                       column.toggleVisibility(!!value)
                     }
                   >
-                    {column.id}
+                    {column.id.replace('_', ' ')}
                   </DropdownMenuCheckboxItem>
                 )
               })}
