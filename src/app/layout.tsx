2db1d022506bb8cfe07c66da59d6ed4d73fa3b5a
Alex Perez <alejandro.gperez745@gmail.com>
2024-03-06 22:02:48 -0600

    SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,30 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { Toaster } from '@/components/ui/toaster'
-import { AuthProvider } from '@/context/authProvider'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from "@/context/authProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'AppTrack',
-  description: 'Keep track of your apps!',
-}
+  title: "AppTrack",
+  description: "Keep track of your apps!",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <AuthProvider>
         <body className={inter.className}>
           {children}
+          <SpeedInsights />
           <Toaster />
         </body>
       </AuthProvider>
     </html>
-  )
+  );
 }
