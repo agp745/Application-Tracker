d7a35b21be725f90ba2ff725c81eac1460f7e671
Alex Perez <alejandro.gperez745@gmail.com>
2024-03-07 13:15:17 -0600

    callback url note at bottom

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,30 +1,46 @@
 import { NextAuthOptions } from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
-import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/queries";
 
 import type { Adapter } from "next-auth/adapters";
 
 export const options: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma) as Adapter,
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENTID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENTID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string
-        })
-    ],
-    pages: {
-        signIn: '/login'
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENTID as string,
+      clientSecret: process.env.GITHUB_SECRET2 as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENTID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = true;
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        // Return false to display a default error message
+        return false;
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
+      }
     },
-    callbacks: {
-        //@ts-expect-error
-        async session({session, user}){
-            return {session, user}
-        }
-    }
-}
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
+};
+
+// https://app-tracker-three.vercel.app/api/auth/callback/github
